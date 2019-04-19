00946677c4a8af3feddc7aeaf777cb6a29b7fffa
David S Grigoryan <david@fyuld.com>
2019-04-19 18:18:37 +0100

    Added Datamodel File
    Added datamodel file `datamodel.ts` with following types:
    
    * Payment
    * PaymentFactory
    * Ledger

---
+++ simple-payments/src/datamodel.ts
@@ -0,0 +1,17 @@
+export type Payment = {
+  date: Date
+  originator: string
+  beneficiary: string
+  amount: number
+}
+
+export type PaymentFactory = (
+  date: Date,
+  originator: string,
+  beneficiary: string,
+  amount: number
+) => Payment
+
+export type Ledger = {
+  payments: Payment[]
+}
