e9ac59b10238219ec299b0a1d9e16c5cb6bfb8b7
David Sergey <me@davidsergey.com>
2020-02-11 09:24:58 +0000

    Added some yet unused enums for swift

---
+++ banky-bank/unused-yet/swift-enums.ts
@@ -0,0 +1,32 @@
+export type Individual = {
+	name: string
+}
+
+export type NaturalPerson = {
+	name: string
+}
+
+export enum PartyIdentifier {
+	AlienRegistrationNumber = 'ARNU',
+	PassportNumber = 'CCPT',
+	CustomerIdentificationNumber = 'CUST',
+	DriverLicenceNumber = 'DRLC',
+	EmployerNumber = 'EMPL',
+	NationalIdentityNumber = 'NIDN',
+	SocialSecurityNumber = 'SOSE',
+	TaxIdentificationNumber = 'TXID',
+	ErrorCode = 'T55',
+}
+
+export enum BankOperationCodes {
+	Cheque = 'CHQB',
+	CreditTransfer = 'CREDIT',
+	TestCreditTransfer = 'CRTST',
+	SwiftPay = 'SPAY',
+}
+
+export enum DetailsOfCharges {
+	Beneficiary = 'BEN',
+	OurCustomer = 'OUR',
+	SharedCharges = 'SHA',
+}
